7950aeed3406cfdd8fbaedf9d4459f82c231f310
Marcos200216 <jr417270@gmail.com>
2025-07-10 22:04:57 -0600

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Desplazamiento suave entre secciones
+
 document.querySelectorAll('.navbar a').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
@@ -8,23 +8,19 @@ document.querySelectorAll('.navbar a').forEach(anchor => {
   });
 });
 
-// Función para abrir el modal
 function openForm() {
   document.getElementById("contactModal").style.display = "block";
 }
 
-// Función para cerrar el modal
 function closeForm() {
   document.getElementById("contactModal").style.display = "none";
 }
 
-// Cierra el modal si se hace clic fuera de él
 window.onclick = function (event) {
   if (event.target == document.getElementById("contactModal")) {
     closeForm();
   }
 };
-
 
 document.addEventListener("DOMContentLoaded", function () {
   let slideIndex = 0;
